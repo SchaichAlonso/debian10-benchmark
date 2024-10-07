fc913c0b2a4e21ba1bafe1f1c00816a3b212dba1
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-10-07 10:22:56 +0000

    paste

--- 1-initial/1.1-filesystems/1.1.6-configure-var-log-audit/1.1.6.3-ensure-noexec-option-is-set.sh
+++ 1-initial/1.1-filesystems/1.1.6-configure-var-log-audit/1.1.6.3-ensure-noexec-option-is-set.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if (findmnt -nk /tmp | grep -qe "noexec"); then
+if (findmnt -nk /var/log/audit | grep -qe "noexec"); then
     echo -e "$(tput setaf 2)PASS $0 $(tput sgr0)"
 else
     echo -e "$(tput setaf 1)FAIL $0 $(tput sgr0)"
--- 1-initial/1.1-filesystems/1.1.8-configure-dev-shm/1.1.8.3-ensure-nosuid-option-is-set.sh
+++ 1-initial/1.1-filesystems/1.1.8-configure-dev-shm/1.1.8.3-ensure-nosuid-option-is-set.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if (findmnt -nk /tmp | grep -qe "nosuid"); then
+if (findmnt -nk /dev/shm | grep -qe "nosuid"); then
     echo -e "$(tput setaf 2)PASS $0 $(tput sgr0)"
 else
     echo -e "$(tput setaf 1)FAIL $0 $(tput sgr0)"
