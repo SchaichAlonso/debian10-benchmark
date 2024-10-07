50e1e826437891a872268a2684ad4378be9a5d90
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-10-07 18:29:01 +0900

    Print audit name, colorized

--- 1-initial/1.1-filesystems/1.1.2-configure-tmp/1.1.2.2-ensure-nodev-option-is-set.sh
+++ 1-initial/1.1-filesystems/1.1.2-configure-tmp/1.1.2.2-ensure-nodev-option-is-set.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if (findmnt -nk /tmp | grep -qe "nodev"); then
-    echo -e "Audit Result: $(tput setaf 2)PASS$(tput sgr0)"
+    echo -e "$(tput setaf 2)PASS $1 $(tput sgr0)"
 else
-    echo -e "Audit Result: $(tput setaf 1)FAIL$(tput sgr0)"
+    echo -e "$(tput setaf 1)FAIL $1 $(tput sgr0)"
 fi
