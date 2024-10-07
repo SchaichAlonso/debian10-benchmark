19356d7dd98132f53ee1cebd2bd5c58da6f68a7c
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-10-07 18:26:59 +0900

    FAIL is red

--- 1-initial/1.1-filesystems/1.1.2-configure-tmp/1.1.2.2-ensure-nodev-option-is-set.sh
+++ 1-initial/1.1-filesystems/1.1.2-configure-tmp/1.1.2.2-ensure-nodev-option-is-set.sh
@@ -3,5 +3,5 @@
 if (findmnt -nk /tmp | grep -qe "nodev"); then
     echo -e "Audit Result: $(tput setaf 2)PASS$(tput sgr0)"
 else
-    echo -e "Audit Result: $(tput setaf 2)FAIL$(tput sgr0)"
+    echo -e "Audit Result: $(tput setaf 1)FAIL$(tput sgr0)"
 fi
